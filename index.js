feda5a9371c21c471d5d32a6e34911da0fe27446
Noela-Murugi <murugin254@gmail.com>
2022-09-05 11:51:47 +0300

    completed practice

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+   }
+
+  static sanitize(string) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '')
+   }
+
+  static titleize(sentence) {
+    let expected = [];
+    let newString =  ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let captSentence = sentence.split( " ")
+    for (let x =0; x < captSentence.length; x++){
+    if (x == 0){
+      expected.push( this.capitalize(captSentence[x]))
+    }else if (newString.includes(captSentence[x])){
+        expected.push(captSentence[x])
+      }else{
+        expected.push(this.capitalize(captSentence[x]))
+      }
+
+  }
+  return expected.join( " ");
 }
+}
+
+Formatter.capitalize();
+
+Formatter.sanitize();
+
+Formatter.titleize();
